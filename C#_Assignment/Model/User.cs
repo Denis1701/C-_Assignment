72fd148c251a971e1c317249f955cdba1608fd50
Dennis Kotsyuba <Deniskotsyuba17@gmail.com>
2023-03-13 01:44:22 +0200

    My Final Commit To The Assignment :)

--- C#_Assignment/Model/User.cs
+++ C#_Assignment/Model/User.cs
@@ -1,22 +1,113 @@
-﻿using System.Text.Json;
+﻿using System.Collections;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace C__Assignment.Model
 {
     public class User
     {
-        public ICollection<string>? Name { get; set; }
-        public ICollection<string>? Gender { get; set; }
-        public ICollection<string>? Location { get; set; }
-        public ICollection<string>? Email { get; set; }
-        public ICollection<string>? Login { get; set; }
-        public ICollection<string>? Registered { get; set; }
-        public ICollection<string>? Dob { get; set; }
-        public ICollection<string>? Phone { get; set; }
-        public ICollection<string>? Cell { get; set; }
-        public ICollection<string>? Id { get; set; }
-        public ICollection<string>? Picture { get; set; }
-        public ICollection<string>? Nat { get; set; }
-        public override string ToString() => JsonSerializer.Serialize<User>(this);
+        public List<Result>? Results { get; set; }
+        public Info? Info { get; set; }
     }
+
+    public class Result
+    {
+        public string? Gender { get; set; }
+        public Name? Name { get; set; }
+        public Location? Location { get; set; }
+        public string? Email { get; set; }
+        public Login? Login { get; set; }
+        public Dob? Dob { get; set; }
+        public Registered? Registered { get; set; }
+        public string? Phone { get; set; }
+        public string? Cell { get; set; }
+        public Id? Id { get; set; }
+        public Picture? Picture { get; set; }
+        public string? Nat { get; set; }
+    }
+
+    public class Name
+    {
+        public string? Title { get; set; }
+        public string? First { get; set; }
+        public string? Last { get; set; }
+    }
+
+    public class Location
+    {
+        public Street? Street { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+        public string? Postcode { get; set; }
+        public Coordinates? Coordinates { get; set; }
+        public Timezone? Timezone { get; set; }
+    }
+
+    public class Street
+    {
+        public int Number { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public class Coordinates
+    {
+        public string? Latitude { get; set; }
+        public string? Longitude { get; set; }
+    }
+
+    public class Timezone
+    {
+        public string? Offset { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class Login
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Salt { get; set; }
+        public string? Md5 { get; set; }
+        public string? Sha1 { get; set; }
+        public string? Sha256 { get; set; }
+    }
+
+    public class Dob
+    {
+        public DateTime Date { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Registered
+    {
+        public DateTime Date { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Id
+    {
+        public string? Name { get; set; }
+        public string? Value { get; set; }
+    }
+
+    public class Picture
+    {
+        public string? Large { get; set; }
+        public string? Medium { get; set; }
+        public string? Thumbnail { get; set; }
+    }
+
+    public class Info
+    {
+        public string? Seed { get; set; }
+        public int Results { get; set; }
+        public int Page { get; set; }
+        public string? Version { get; set; }
+    }
+
+
+
+
+
 }
+
